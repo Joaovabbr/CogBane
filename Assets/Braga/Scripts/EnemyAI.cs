8a1d07332f11e240f49908d3920c22fd2c35ae18
João Victor Braga <joaovab@al.insper.edu.br>
2026-05-03 20:05:10 -0300

    feat: lobo incompleto (animações)

--- Assets/Braga/Scripts/EnemyAI.cs
+++ Assets/Braga/Scripts/EnemyAI.cs
@@ -9,30 +9,44 @@ public class EnemyAI : MonoBehaviour
     public GameObject pointA;
     public GameObject pointB;
     private Transform currentPoint;
-    private bool isPatrolling;
+    private string state;
     public float patrolStopDuration;
+    private float distance;
     private float cronometroPatrolStop;
     private bool isStopped;
-    
 
-    private float distance;
+    public bool dead;
+    // controle de ataque
+    private float CronometroAtack;
+    
+    // controle de animação
+    private Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb =  GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         currentPoint = pointB.transform;
-        isPatrolling = true;
+        state = "patrolling";
         cronometroPatrolStop = 0.0f;
+        CronometroAtack = 0.0f;
+        dead = false;
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {  
+        print(state);
+        // define state
         distance  = Vector2.Distance(transform.position, player.transform.position );
-        if (distance <= 4) isPatrolling = false;
-        if (distance > 4) isPatrolling = true;
-        if (isPatrolling)
+        if (distance <= 4 && state != "dead") state = "running";
+        if (distance > 4 && state != "dead") state = "patrolling";
+        if (state == "running" && distance <= 1.7 && state != "dead") state = "attacking";
+        
+        // ação de state
+        if (state == "patrolling")
         {
+            anim.SetFloat("speed", patrolSpeed);
             if (!isStopped)
             {
                 cronometroPatrolStop = 0.0f;
@@ -40,10 +54,12 @@ public class EnemyAI : MonoBehaviour
             if (currentPoint == pointB.transform)
             {
                 rb.linearVelocity = new Vector2(patrolSpeed, 0);
+                transform.eulerAngles = new Vector3(0, 0, 0);
             }
             else
             {
                 rb.linearVelocity = new Vector2(-patrolSpeed, 0);
+                transform.eulerAngles = new Vector3(0, 180f, 0);
             }
 
             if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
@@ -51,11 +67,12 @@ public class EnemyAI : MonoBehaviour
                 
                 cronometroPatrolStop +=  Time.deltaTime;
                 patrolSpeed = 0;
-                print(cronometroPatrolStop + " B");
+                anim.SetFloat("speed", patrolSpeed);
                 isStopped = true;
                 if (cronometroPatrolStop >= patrolStopDuration)
                 {
                     patrolSpeed = 0.75f;
+                    anim.SetFloat("speed", patrolSpeed);
                     isStopped = false;
                     currentPoint = pointA.transform;
                 }
@@ -63,24 +80,46 @@ public class EnemyAI : MonoBehaviour
             }
             if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
             {
-                
                 cronometroPatrolStop +=  Time.deltaTime;
                 patrolSpeed = 0;
-                print(cronometroPatrolStop+ " A");
+                anim.SetFloat("speed", patrolSpeed);
                 isStopped = true;
                 if (cronometroPatrolStop >= patrolStopDuration)
                 {
                     patrolSpeed = 0.75f;
+                    anim.SetFloat("speed", patrolSpeed);
                     isStopped = false;
                     currentPoint = pointB.transform;
                 }
             }
         }
+        else if (state == "attacking")
+        {
+           if (CronometroAtack == 0) anim.SetTrigger("attack");
+           CronometroAtack += Time.deltaTime;
+           if (CronometroAtack >= 1)
+           {
+               state = "running";
+               CronometroAtack = 0;
+           }
+            
+        }
+        else if (state == "dead")
+        {
+            if (!dead) anim.SetTrigger("death");
+            followingSpeed = 0;
+            patrolSpeed = 0;
+            dead = true;
+        }
         else
         {
             Vector2 direction = player.transform.position - transform.position;
+            if (direction.x > 0) transform.eulerAngles = new Vector3(0, 0, 0);
+            if  (direction.x < 0) transform.eulerAngles = new Vector3(0, 180, 0);
             transform.position  = Vector2.MoveTowards(this.transform.position, player.transform.position, followingSpeed * Time.deltaTime);
+            anim.SetFloat("speed", followingSpeed);
         }
+        
         
         
     }
